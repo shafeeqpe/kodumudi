aa87462481633cb0ea280b88fc2e3d99d8ab3c8d
shafeeqpe <shafeeqpem@gmail.com>
2018-11-22 18:48:28 +0530

    first app

---
+++ precache-manifest.ad14e1fd0a5681d3364f504b4d4764fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a396daaafab96986679",
+    "url": "/static/js/main.8a396daa.chunk.js"
+  },
+  {
+    "revision": "6c58186ad0ce379b81d5",
+    "url": "/static/js/1.6c58186a.chunk.js"
+  },
+  {
+    "revision": "8a396daaafab96986679",
+    "url": "/static/css/main.50ffe555.chunk.css"
+  },
+  {
+    "revision": "0c6e17ae35259e2b3b76eba1235348f7",
+    "url": "/index.html"
+  }
+];
